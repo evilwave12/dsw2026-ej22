62912fc4225a061c321f788a3de969d794ba3138
IanGiuliante <ian.giuliante@gmail.com>
2026-09-22 15:27:59 -0300

    funcionamiento de botones submit y show password

--- login.js
+++ login.js
@@ -1,11 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
-    
+    const usernameInput=document.getElementById('username');
+    const passwordInput=document.getElementById('password');
+
+    const togglePasswordBtn = document.getElementById('ocultaContraseñas');
+    const eyeOpenIcon = togglePasswordBtn.querySelector('.open-eye');
+    const eyeTachadoIcon = togglePasswordBtn.querySelector('.tachado-eye');
+
+
+    togglePasswordBtn.addEventListener('click', function() {
+        //const isPassword = passwordInput.type==='password';
+        if(passwordInput.type==="password"){
+            passwordInput.type="text";
+            eyeOpenIcon.classList.add('hidden');
+            eyeTachadoIcon.classList.remove('hidden');
+        }else{
+            passwordInput.type='password';
+            eyeOpenIcon.classList.remove('hidden');
+            eyeTachadoIcon.classList.add('hidden');
+        }
+    });
+
+
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+        const username = usernameInput.value;
+        const password = passwordInput.value;
         
         // Aquí puedes agregar la lógica para validar el usuario y la contraseña
         if(username === 'admin' && password === 'password') {
